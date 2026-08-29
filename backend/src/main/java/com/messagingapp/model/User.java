8903c772066103e4c779ce951888abd7f97d1b56
Jim Foo <jimfoo88.ck@gmail.com>
2026-08-30 02:42:43 +0800

    update the dockerfile to use mvn docker and user.java

--- backend/src/main/java/com/messagingapp/model/User.java
+++ backend/src/main/java/com/messagingapp/model/User.java
@@ -1,3 +1,3 @@
 package com.messagingapp.model;
-import org.springframework.data.annotation.Id; import org.springframework.data.mongodb.core.index.Indexed; import org.springframework.data.mongodb.core.mapping.Document;
-@Document("users") public record User(@Id String id, @Indexed(unique=true) String username, String displayName, String passwordHash) {}
+import com.fasterxml.jackson.annotation.JsonIgnore; import org.springframework.data.annotation.Id; import org.springframework.data.mongodb.core.index.Indexed; import org.springframework.data.mongodb.core.mapping.Document;
+@Document("users") public record User(@Id String id, @Indexed(unique=true) String username, String displayName, @JsonIgnore String passwordHash) {}
